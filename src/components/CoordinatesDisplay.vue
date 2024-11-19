8eb619d1db38054f3f32cc839740a04f1e07540b
b.kataev <buvaysar2032@gmail.com>
2024-11-19 10:35:46 +0300

    initial commit

---
+++ src/components/CoordinatesDisplay.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  xCoord: Number,
+  yCoord: Number
+});
+</script>
+
+<template>
+  <p>
+    Координаты:
+    X: <span>{{ xCoord }}</span>
+    Y: <span>{{ yCoord }}</span>
+  </p>
+</template>
+
+<style scoped>
+
+</style>
